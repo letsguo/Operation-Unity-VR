7336099c629941f5e13704f7798ea846a2d13d36
letsguo <Letsguo@Gabes-MacBook-Pro.local>
2020-08-14 12:53:42 -0700

    First Commit

---
+++ Assets/ShootIfGrabbed.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShootIfGrabbed : MonoBehaviour
+{
+    private SimpleShoot simpleShoot;
+    private OVRGrabbable ovrGrabbable;
+    public OVRInput.Button shootingButton;
+    public int maxNumberOfBullet = 1000;
+    public Text bulletText;
+    public AudioClip shootingAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        simpleShoot = GetComponent<SimpleShoot>();
+        ovrGrabbable = GetComponent<OVRGrabbable>();
+        bulletText.text = maxNumberOfBullet.ToString();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ovrGrabbable.isGrabbed && OVRInput.GetDown(shootingButton, ovrGrabbable.grabbedBy.GetController()) && maxNumberOfBullet > 0)
+        {
+            //shoot
+            GetComponent<AudioSource>().PlayOneShot(shootingAudio);
+            simpleShoot.TriggerShoot(); 
+            maxNumberOfBullet--;
+            bulletText.text = maxNumberOfBullet.ToString();
+
+        }
+        
+    }
+}
